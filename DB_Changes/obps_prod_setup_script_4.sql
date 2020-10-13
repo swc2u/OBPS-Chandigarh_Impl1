b7964447d8560934b02d0194c47fd495e9939ce7
Md Reza Khan <rezakhan9494@gmail.com>
2020-10-13 16:28:59 +0530

    OBPS-UAT-24-  word limit increase by 3750 char for comments

--- DB_Changes/obps_prod_setup_script_4.sql
+++ DB_Changes/obps_prod_setup_script_4.sql
@@ -1,9 +1,9 @@
 ---Comments size changes--
 
-ALTER TABLE chandigarh.eg_wf_state_history ALTER COLUMN "comments" type varchar(3750);
+ALTER TABLE chandigarh.eg_wf_state_history ALTER COLUMN "comments" type varchar(4000);
 
-ALTER TABLE state.eg_wf_state_history ALTER COLUMN "comments" type varchar(3750);
+ALTER TABLE state.eg_wf_state_history ALTER COLUMN "comments" type varchar(4000);
 
-ALTER TABLE chandigarh.eg_wf_states ALTER COLUMN "comments" type varchar(3750);
+ALTER TABLE chandigarh.eg_wf_states ALTER COLUMN "comments" type varchar(4000);
 
-ALTER TABLE state.eg_wf_states ALTER COLUMN "comments" type varchar(3750);
+ALTER TABLE state.eg_wf_states ALTER COLUMN "comments" type varchar(4000);
