7a92efdd994804fb5bcbff27e200e7e82a002885
Debjit Ghar <debjit.ghar@gmail.com>
2020-12-04 21:01:16 +0530

    Changes in report

---
+++ egov/egov-bpa/src/main/java/org/egov/bpa/transaction/entity/dto/SearchPendingItemsForm.java
@@ -0,0 +1,192 @@
+package org.egov.bpa.transaction.entity.dto;
+
+import java.util.Date;
+import org.egov.bpa.transaction.entity.BpaApplication;
+import org.egov.bpa.transaction.entity.oc.OccupancyCertificate;
+import org.egov.infra.web.support.search.DataTableSearchRequest;
+import org.hibernate.validator.constraints.SafeHtml;
+
+public class SearchPendingItemsForm extends DataTableSearchRequest {
+	private Long id;
+    @SafeHtml
+    private String applicationNumber;
+    private Date applicationDate;
+    private Long applicationTypeId;
+    @SafeHtml
+    private String applicantType;
+    private Long serviceTypeId;
+    @SafeHtml
+    private String serviceType;
+    @SafeHtml
+    private String currentOwner;    
+    @SafeHtml
+    private String occupancy;
+    @SafeHtml
+    private String serviceCode;
+    private Date fromDate;
+    private Date toDate;
+    @SafeHtml
+    private String status;   
+    @SafeHtml
+    private String applicantName;    
+    @SafeHtml
+    private String pendingAction; 
+    @SafeHtml
+    private String applicationType;
+    private int ellapseTime;
+    @SafeHtml
+    private String currentOwnerDesg; 
+    
+    public SearchPendingItemsForm() {}
+
+	public SearchPendingItemsForm(BpaApplication application, String currentOwnerName, String currentOwnerDesg, String pendingAction, int ellapseDays) {
+        setId(application.getId());
+        setApplicationNumber(application.getApplicationNumber());
+        setApplicantName(application.getApplicantName());
+        setApplicationDate(application.getApplicationDate());
+        setApplicationType(application.getApplicationType() != null ? application.getApplicationType().getDescription() : "");
+        setOccupancy(application.getOccupanciesName());
+        setServiceType(application.getServiceType().getDescription());
+        setServiceCode(application.getServiceType().getCode());        
+        setStatus(application.getStatus().getCode());
+        setCurrentOwner(currentOwnerName);
+        setCurrentOwnerDesg(currentOwnerDesg);
+        setPendingAction(pendingAction);
+        setEllapseTime(ellapseDays);
+    }
+	
+	 public SearchPendingItemsForm(OccupancyCertificate occupancyCertificate, String currentOwnerName, String currentOwnerDesg, String pendingAction, int ellapseDays) {
+        setId(occupancyCertificate.getId());
+        setApplicationNumber(occupancyCertificate.getApplicationNumber());
+        setApplicantName(occupancyCertificate.getParent().getOwner().getName());
+        setApplicationDate(occupancyCertificate.getApplicationDate());
+        setApplicationType(occupancyCertificate.getParent().getApplicantType());
+        setOccupancy(occupancyCertificate.getParent().getOccupanciesName());
+        setServiceType(occupancyCertificate.getParent().getServiceType().getDescription());
+        setServiceCode(occupancyCertificate.getParent().getServiceType().getCode());
+        setStatus(occupancyCertificate.getStatus().getCode());
+        setCurrentOwner(currentOwnerName);
+        setCurrentOwnerDesg(currentOwnerDesg);
+        setPendingAction(pendingAction);
+        setEllapseTime(ellapseDays);
+	}
+    
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getApplicationNumber() {
+		return applicationNumber;
+	}
+	public void setApplicationNumber(String applicationNumber) {
+		this.applicationNumber = applicationNumber;
+	}
+	public Date getApplicationDate() {
+		return applicationDate;
+	}
+	public void setApplicationDate(Date applicationDate) {
+		this.applicationDate = applicationDate;
+	}
+	public String getApplicantType() {
+		return applicantType;
+	}
+	public void setApplicantType(String applicantType) {
+		this.applicantType = applicantType;
+	}
+	public String getServiceType() {
+		return serviceType;
+	}
+	public void setServiceType(String serviceType) {
+		this.serviceType = serviceType;
+	}
+	public String getCurrentOwner() {
+		return currentOwner;
+	}
+	public void setCurrentOwner(String currentOwner) {
+		this.currentOwner = currentOwner;
+	}
+	public String getOccupancy() {
+		return occupancy;
+	}
+	public void setOccupancy(String occupancy) {
+		this.occupancy = occupancy;
+	}
+	public String getServiceCode() {
+		return serviceCode;
+	}
+	public void setServiceCode(String serviceCode) {
+		this.serviceCode = serviceCode;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getApplicantName() {
+		return applicantName;
+	}
+	public void setApplicantName(String applicantName) {
+		this.applicantName = applicantName;
+	}
+	public String getPendingAction() {
+		return pendingAction;
+	}
+	public void setPendingAction(String pendingAction) {
+		this.pendingAction = pendingAction;
+	}
+	public String getApplicationType() {
+		return applicationType;
+	}
+	public void setApplicationType(String applicationType) {
+		this.applicationType = applicationType;
+	}
+	public int getEllapseTime() {
+		return ellapseTime;
+	}
+	public void setEllapseTime(int ellapseTime) {
+		this.ellapseTime = ellapseTime;
+	}
+
+	public Long getApplicationTypeId() {
+		return applicationTypeId;
+	}
+
+	public void setApplicationTypeId(Long applicationTypeId) {
+		this.applicationTypeId = applicationTypeId;
+	}
+
+	public Long getServiceTypeId() {
+		return serviceTypeId;
+	}
+
+	public void setServiceTypeId(Long serviceTypeId) {
+		this.serviceTypeId = serviceTypeId;
+	}
+
+	public Date getFromDate() {
+		return fromDate;
+	}
+
+	public void setFromDate(Date fromDate) {
+		this.fromDate = fromDate;
+	}
+
+	public Date getToDate() {
+		return toDate;
+	}
+
+	public void setToDate(Date toDate) {
+		this.toDate = toDate;
+	}
+
+	public String getCurrentOwnerDesg() {
+		return currentOwnerDesg;
+	}
+
+	public void setCurrentOwnerDesg(String currentOwnerDesg) {
+		this.currentOwnerDesg = currentOwnerDesg;
+	}	
+}
---
+++ egov/egov-bpaweb/src/main/webapp/resources/js/app/searchpendingitems.js
@@ -0,0 +1,145 @@
+$(document).ready(function() {	
+	$('#btnSearch').click(function() {
+		var isValid = false;
+        $('#searchPendingItemsForm').find(':input',':select',':textarea').each(function() {
+            if($(this).val()) {
+                isValid =true;
+                return false;
+			} else
+                isValid =false;
+        });
+        if(isValid) {
+            callAjaxSearch();
+        } else {
+        	bootbox.alert($('#atleastOneInputReq').val());
+            return false;
+        }
+    });    
+});
+
+function callAjaxSearch() {
+	var viewurl = '/bpa/application/view/';
+	$('.report-section').removeClass('display-hide');
+	$("#search_bpa_pending_items_table").dataTable({
+		processing: true,
+		serverSide: true,
+		sort: true,
+		filter: true,
+		"searching": false,
+		responsive: true,
+		rowReorder: true,
+		"order": [[1, 'asc']],
+		ajax : {
+			url : "/bpa/application/searchPendingItems",
+			type : "POST",
+			beforeSend : function() {
+				$('.loader-class').modal('show',{backdrop : 'static'});
+			},
+			data: function (args) {
+				return {
+					"args": JSON.stringify(args),
+					"serviceTypeId": $("#serviceTypeId").val(),
+					"applicationTypeId": $("#applicationTypeId").val(),
+					"currentOwnerDesg":$("#currentOwnerDesg").val(),
+					"applicantName": $("#applicantName").val(),
+					"applicationNumber": $("#applicationNumber").val(),
+					"fromDate": $("#fromDate").val(),
+					"toDate": $("#toDate").val()
+				};
+			},
+			complete : function() {
+				$('.loader-class').modal('hide');
+			}
+		},
+		"bDestroy" : true,
+		"sDom" : "<'row'<'col-xs-12 hidden col-right'f>r>t<'row'<'col-xs-3'i><'col-xs-3 col-right'l><'col-xs-3 col-right'<'export-data'T>><'col-xs-3 text-right'p>>",
+		"oTableTools" : {
+			"sSwfPath" : "../../../../../../egi/resources/global/swf/copy_csv_xls_pdf.swf",
+			"aButtons" : [ "xls", "pdf",
+							"print" ]
+		},
+		aaSorting : [],
+		columns : [
+			{
+				"data" : "applicationType",
+				"sClass" : "text-left"
+			},
+			{
+				"data" : "applicantName",
+				"sClass" : "text-left"
+			},
+			{
+				"data" : "applicationNumber",
+				"sClass" : "text-left"
+			},
+
+			{
+				"data" : "applicationDate",
+				"sClass" : "text-left",
+				render: function (data) {
+					return data.split("-").reverse().join("/");
+				}
+			},
+			{
+				"data" : "serviceType",
+				"sClass" : "text-left"
+			},
+			{
+				"data" : "occupancy",
+				"sClass" : "text-left"
+			},
+			{
+				"data" : "status",
+				"sClass" : "text-left"
+			},
+			{
+				"data" : "currentOwner",
+				"sClass" : "text-left"
+			},
+			{
+				"data" : "currentOwnerDesignation",
+				"sClass" : "text-left"
+			},
+			{
+				"data" : "pendingAction",
+				"sClass" : "text-left"
+			},
+			{
+				"data" : "ellapseTime",
+				"sClass" : "text-left"
+			},
+			{
+				"data" : null,
+				"sClass" : "text-left",
+				"render" : function(data, type, row, meta) {    
+					var commonOptions = '<option value="">---Select an Action----</option><option  value=' + viewurl + row.applicationNumber + '>View</option>';
+					return ('<select class="dropchange" style="width:160px;font-size: small">'+commonOptions+'></select>');
+				}
+			} 
+		]
+	});	
+}					
+					
+$(document).on('change','.dropchange',function(){
+	var url = $(this).val();
+	if(url){
+	   openPopup(url);
+	}
+	// reset dropdown value to default
+    $('.dropchange').val('');
+});
+
+function openPopup(url){
+	window.open(url,'window','scrollbars=yes,resizable=yes,height=700,width=800,status=yes');
+}
+
+function getFormData($form) {
+	var unindexed_array = $form.serializeArray();
+	var indexed_array = {};
+
+	$.map(unindexed_array, function(n, i) {
+		indexed_array[n['name']] = n['value'];
+	});
+
+	return indexed_array;
+}
