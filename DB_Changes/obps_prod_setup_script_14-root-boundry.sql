039433bea2b75088bec6389a0231f59ebe2030bb
Md Reza Khan <rezakhan9494@gmail.com>
2022-04-29 14:03:34 +0000

    sector 8A,B,C added in prod and dev

---
+++ DB_Changes/obps_prod_setup_script_14-root-boundry.sql
@@ -0,0 +1,12 @@
+
+INSERT INTO chandigarh.eg_boundary (id,boundarynum,parent,"name",boundarytype,localname,bndry_name_old,bndry_name_old_local,fromdate,todate,bndryid,longitude,latitude,materializedpath,active,createddate,lastmodifieddate,createdby,lastmodifiedby,"version",code) VALUES
+	 (nextval('seq_eg_boundary'),1,(select id from chandigarh.eg_boundary where name='Center'),'8A',11,'8A',NULL,NULL,'2019-04-01 00:00:00.000','2099-03-31 00:00:00.000',NULL,NULL,NULL,NULL,true,now(),'2021-02-11 12:45:19.840',1,1,0,'8A');
+	 
+	 
+INSERT INTO chandigarh.eg_boundary (id,boundarynum,parent,"name",boundarytype,localname,bndry_name_old,bndry_name_old_local,fromdate,todate,bndryid,longitude,latitude,materializedpath,active,createddate,lastmodifieddate,createdby,lastmodifiedby,"version",code) VALUES
+	 (nextval('seq_eg_boundary'),1,(select id from chandigarh.eg_boundary where name='Center'),'8B',11,'8B',NULL,NULL,'2019-04-01 00:00:00.000','2099-03-31 00:00:00.000',NULL,NULL,NULL,NULL,true,now(),'2021-02-11 12:45:19.840',1,1,0,'8B');
+	 
+	 
+INSERT INTO chandigarh.eg_boundary (id,boundarynum,parent,"name",boundarytype,localname,bndry_name_old,bndry_name_old_local,fromdate,todate,bndryid,longitude,latitude,materializedpath,active,createddate,lastmodifieddate,createdby,lastmodifiedby,"version",code) VALUES
+	 (nextval('seq_eg_boundary'),1,(select id from chandigarh.eg_boundary where name='Center'),'8C',11,'8C',NULL,NULL,'2019-04-01 00:00:00.000','2099-03-31 00:00:00.000',NULL,NULL,NULL,NULL,true,now(),'2021-02-11 12:45:19.840',1,1,0,'8C');
+
