972f4c45d14e0f8f9139985407da5bf25fcc7b37
kaustubhTranserve <kaustubh.katti@transerve.com>
2022-05-09 18:24:07 +0530

    comment added for reference to reza

--- DB_Changes/obps_prod_setup_script_20-delete-duplicate-OF-feemapping.sql
+++ DB_Changes/obps_prod_setup_script_20-delete-duplicate-OF-feemapping.sql
@@ -1,3 +1,7 @@
+-- This is to delete BPA FEE COMMON - Code OF which is mapped twice in 'egbpa_mstr_bpafeemapping'
+-- This Entries are used as a foreign key constraint in table egbpa_application_feedetails and need to be removed
+-- from there first before deleting it from the 'egbpa_mstr_bpafeemapping' table
+
 DELETE FROM chandigarh.egbpa_application_feedetails 
 WHERE bpafeemapping = 164;
 DELETE FROM chandigarh.egbpa_application_feedetails 
