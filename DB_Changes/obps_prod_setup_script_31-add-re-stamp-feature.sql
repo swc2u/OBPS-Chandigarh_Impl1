af2a068d0147efc70a8809b81b958f2d92b845d8
sunitha <sunitha.ranjith@transerve.com>
2022-08-11 13:51:19 +0530

    modified script to provide restamp access to admin

--- DB_Changes/obps_prod_setup_script_31-add-re-stamp-feature.sql
+++ DB_Changes/obps_prod_setup_script_31-add-re-stamp-feature.sql
@@ -1,7 +1,10 @@
-	INSERT INTO chandigarh.eg_feature(
-	id, name, description, module, version)
-	VALUES (Nextval('chandigarh.seq_eg_feature'), 'Re-stamp documents', 'Re-stamp documents', 1, 0);
-	
-INSERT INTO chandigarh.eg_feature_role(
-	role, feature)
-	VALUES (5, (select id from chandigarh.eg_feature where name='Re-stamp documents'));
+INSERT INTO chandigarh.eg_module (id, name, enabled, contextroot, parentmodule, displayname, ordernumber) 
+VALUES (nextval('chandigarh.seq_eg_module'), 'Re-stamp', true, 'bpa', 444, 'Re-stamp BPA documents', 1);
+
+
+INSERT INTO chandigarh.eg_action (id, name, url, queryparams, parentmodule, ordernumber, displayname, enabled, contextroot, version, createdby, createddate, lastmodifiedby, lastmodifieddate, application) 
+VALUES  (nextval('chandigarh.seq_eg_action'), 'Re-stamp BPA documents', '/reStamp', NULL, (select id from chandigarh.eg_module where name='Re-stamp'), 1, 'Re-stamp BPA documents', true, 'bpa', 0, 1, now(), 1, now(), (select id from chandigarh.eg_module where name='Re-stamp'));
+
+
+INSERT INTO chandigarh.eg_roleaction(roleid, actionid)
+VALUES (5, (select id from chandigarh.eg_action where name='Re-stamp BPA documents'));
